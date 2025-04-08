66b32a619287f81c279fbd5fe581e82db6b29e03
lawhead <lawhead@ohsu.edu>
2025-04-07 17:24:28 -0700

    CIS-3122 ; added initial classes

---
+++ src/main/java/org/octri/common/domain/AbstractEntity.java
@@ -0,0 +1,136 @@
+package org.octri.common.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.Version;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AbstractEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    protected Long id;
+
+    @Version
+    protected Integer version;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(updatable = false)
+    protected Date createdAt;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    protected Date updatedAt;
+
+    @LastModifiedBy
+    protected String updatedBy;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((version == null) ? 0 : version.hashCode());
+        result = prime * result + ((createdAt == null) ? 0 : createdAt.hashCode());
+        result = prime * result + ((updatedAt == null) ? 0 : updatedAt.hashCode());
+        result = prime * result + ((updatedBy == null) ? 0 : updatedBy.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AbstractEntity other = (AbstractEntity) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (version == null) {
+            if (other.version != null)
+                return false;
+        } else if (!version.equals(other.version))
+            return false;
+        if (createdAt == null) {
+            if (other.createdAt != null)
+                return false;
+        } else if (!createdAt.equals(other.createdAt))
+            return false;
+        if (updatedAt == null) {
+            if (other.updatedAt != null)
+                return false;
+        } else if (!updatedAt.equals(other.updatedAt))
+            return false;
+        if (updatedBy == null) {
+            if (other.updatedBy != null)
+                return false;
+        } else if (!updatedBy.equals(other.updatedBy))
+            return false;
+        return true;
+    }
+
+}
---
+++ src/main/java/org/octri/common/view/EnumSelectOption.java
@@ -0,0 +1,38 @@
+package org.octri.common.view;
+
+import java.util.Collection;
+
+/**
+ * Used for UI select inputs. Wraps the choice along with its selected status.
+ *
+ * @param<T>
+ */
+public class EnumSelectOption<T extends Enum<T> & Labelled> extends SelectOption<T> {
+
+    /**
+     * Constructor
+     *
+     * @param choice
+     *            - Enum item
+     * @param selected
+     *            - The selected item; may be null
+     */
+    public EnumSelectOption(T choice, T selected) {
+        super(choice, selected);
+        this.setLabel(choice.getLabel());
+    }
+
+    /**
+     * Constructor used for an option in a multi-select.
+     *
+     * @param choice
+     *            - Enum item
+     * @param selected
+     *            - collection of selected items
+     */
+    public EnumSelectOption(T choice, Collection<T> selected) {
+        super(choice, selected);
+        this.setLabel(choice.getLabel());
+    }
+
+}
